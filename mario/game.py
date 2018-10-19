459b2fbd24cdccf0ab9f5fa93ea31dc1a00c0e86
Stephen Cowley <stephencowley44@gmail.com>
2018-10-19 15:46:25 -0600

    Basic game setup

--- mario/game.py
+++ mario/game.py
@@ -4,9 +4,30 @@ import sys
 import os
 
 
+#Move to settings
+WIDTH = 500
+HEIGHT = 500
+
 def Game:
     """Mario game object"""
     def __init__(self):
+        # Set up screen
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+        # Set up caption
+        pg.display.set_caption('MARIO')
+
+        # Set up clock
+        self.clock = pg.time.Clock()
+
+        self.load_data()
+
+    def load_data(self):
+        # Set up folder
+
+        # Load images
+
+        # Load sounds
         pass
 
     def run(self):
@@ -21,12 +42,18 @@ def Game:
     def events(self):
         pass
 
-    def load_data(self):
-        pass
-
     def new(self):
         pass
 
     def load_level(self):
         pass
+
+
+if __name__ == '__main__':
+    # Init pygame
+    pygame.init()
+
+    # Init mixer
+    pygame.mixer.init()
+
 
