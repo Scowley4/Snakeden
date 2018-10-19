c287d0d1e722f568e53ac5ac2750586dca70d953
Stephen Cowley <StephenCowley44@gmail.com>
2018-10-19 13:52:54 -0600

    Initial mario commit

---
+++ mario/player.py
@@ -0,0 +1,11 @@
+class Sprite(pg.sprite.Sprite):
+    """A Sprite Class"""
+    def __init__(self):
+        self._layer = 1
+        self.groups = ()
+        pg.sprite.Sprite.__init__(self, self.groups)
+
+    def update(self):
+        pass
+
+
