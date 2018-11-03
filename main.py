490d2c5c9b4ddacf8efc4e0d6a57f46d0176cf3a
Dillon Astin <astindillon@gmail.com>
2018-11-02 21:38:36 -0600

    setup mario main menu

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import menu
 
 
 def main():
-    arcade = menu.Menu
+    arcade = menu.Menu()
     game_selection = arcade.start_screen()
     if game_selection == 'mario':
         pass
 
 
-if __name__ == '__main':
+if __name__ == '__main__':
     main()
 
