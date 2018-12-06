4a0cd77afd6c2493f8df6f893ee7dd82f6aa8169
Dillon Astin <astindillon@gmail.com>
2018-12-06 15:10:45 -0700

    added mario running animation

--- mario/mario.py
+++ mario/mario.py
@@ -16,21 +16,32 @@ class Mario(pg.sprite.Sprite):
 
         # single image from sprite, to start
         self.load_images()
-        self.image = self.standing_frames[0]
+        self.image = self.standing_frame_r
         self.rect = self.image.get_rect()
+        self.last_update = 0
+        self.current_frame = 0
 
         self.pos = vec(settings.WIDTH * 1/6, settings.HEIGHT * .8)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+        self.jumping = False
 
     def load_images(self):
-        self.standing_frames = [self.get_image(178, 32, 12, 16),
-                                pg.transform.flip(self.get_image(178, 32, 12, 16), True, False)]
-        for frame in self.standing_frames:
-            frame.set_colorkey(settings.BLACK)
+        self.standing_frame_l = self.get_image(178, 32, 12, 16)
+        self.standing_frame_l.set_colorkey(settings.BLACK)
 
-        self.walk_frames_r = [self.get_image(80, 32, 15, 17), self.get_image(95, 32, 15, 17),
+        self.standing_frame_r = pg.transform.flip(self.standing_frame_l, True, False)
+
+        self.walk_frames_l = [self.get_image(80, 32, 15, 17), self.get_image(95, 32, 15, 17),
                               self.get_image(114, 32, 15, 16)]
+        self.walk_frames_r = []
+        for frame in self.walk_frames_l:
+            frame.set_colorkey(settings.BLACK)
+            self.walk_frames_r.append(pg.transform.flip(frame, True, False))
+        self.jump_frame_l = self.get_image(144, 32, 16, 16)
+        self.jump_frame_l.set_colorkey(settings.BLACK)
+
+        self.jump_frame_r = pg.transform.flip(self.jump_frame_l, True, False)
 
     def reset(self):
         self.pos = vec(settings.WIDTH * 1/6, settings.HEIGHT * .8)
@@ -44,9 +55,29 @@ class Mario(pg.sprite.Sprite):
         self.rect.y -= 1
         # if there is something below us
         if hits:
+            self.jumping = True
             self.vel.y = -8*settings.SCALE
 
+    def animate(self):
+        now = pg.time.get_ticks()
+        if self.vel.x != 0:
+            self.walking = True
+        else:
+            self.walking = False
+        if self.walking:
+            if now - self.last_update > 100:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.walk_frames_r)
+                bottom = self.rect.bottom
+                if self.vel.x < 0:
+                    self.image = self.walk_frames_r[self.current_frame]
+                else:
+                    self.image = self.walk_frames_l[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+
     def update(self):
+        self.animate()
         # 0.5 creates gravity each frame
         self.acc = vec(0, settings.GRAVITY)
         keys = pg.key.get_pressed()
